1408c369c95a835ec7c0f7d6abd620f407fab60f
Daniel <gohan@vp.pl>
2025-12-14 11:17:51 +0100

    Initial Commit

---
+++ MessengerWrapper/MessengerWrapperApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerWrapperApp.swift
+//  MessengerWrapper
+//
+//  Created by Daniel Wyrzyński on 14/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct MessengerWrapperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
